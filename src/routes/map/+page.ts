801ecb20ff461434cf50f6bfbbf2cc3feb4f4979
Felix Diez <diez.felix@gmail.com>
2025-10-11 22:08:36 +0200

    update more deps

--- src/routes/map/+page.ts
+++ src/routes/map/+page.ts
@@ -4,12 +4,15 @@ import { createClients } from '$lib/service/connect';
 
 export const ssr = false;
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const load: PageLoad = async ({ fetch, params, parent }) => {
 	const masterdataClient = createClients(fetch).masterdataClient;
 	const monsterClient = new MonsterClient(fetch);
+	const p = await parent();
+	const user = p.user;
+
 
 	return {
 		cities: (await masterdataClient.getCities({})).cities,
-		monsters: await monsterClient.getMonsters(),
+		monsters: await monsterClient.getMonsters(user.userId),
 	};
 };
